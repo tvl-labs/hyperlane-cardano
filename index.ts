02a417f6b74ec415f71306c55fe7982d8288b727
Hai Nguyen <hai@kreate.community>
2023-06-05 17:56:41 +0700

    feat: emulate a tx locally

--- index.ts
+++ index.ts
@@ -1,31 +1,56 @@
 import * as helios from "@hyperionbt/helios";
+import paramsPreview from "./data/cardano-preview-params.json";
 import MintingPolicyIsmMultiSig from "./src/onchain/ismMultiSig.hl";
 
+const network = new helios.NetworkEmulator();
+const wallets = [1, 2, 3].map((_) => network.createWallet(10_000_000n));
+const messageAddress = network.createWallet().address;
+
+await network.tick(1n);
+
 const ismMultiSig = new MintingPolicyIsmMultiSig({
-  PK_OWNERS: [
-    new helios.PubKey(
-      "0000000000000000000000000000000000000000000000000000000000000000"
-    ),
-    new
-     helios.PubKey(
-      "1111111111111111111111111111111111111111111111111111111111111111"
-    ),
-    new helios.PubKey(
-      "2222222222222222222222222222222222222222222222222222222222222222"
-    ),
-  ],
-  NUM_SIGNATURES_REQUIRED: BigInt(2),
-  ADDR_MESSAGE: new helios.Address(
-    "addr_test1qr2argsphqkfrmcgdzp9czxqfm8qqa77w8jrnv92fs0a9p5tsf75ekkp5ss45xg6twpgz773nr8h55mqc22j0j5pak9sky4g07"
-  ),
-}).compile(true);
+  PK_OWNERS: wallets.map((w) => w.pubKey),
+  NUM_SIGNATURES_REQUIRED: BigInt(Math.floor(wallets.length / 2) + 1),
+  ADDR_MESSAGE: messageAddress,
+}).compile(false);
 
-if (
-  ismMultiSig.mintingPolicyHash.hex !==
-  "cbf3f2ec63333023a30be96dbc707c7357792c90971d38bdd728c7b9"
-) {
-  throw new Error("invalid minting policy hash");
-}
+const message = helios.textToBytes("Hello world from Hyperlane <> Cardano!");
+const messageInUPLC = new helios.ByteArray(message)._toUplcData();
+const signatures = wallets.map((w) =>
+  new helios.ByteArray(
+    helios.Crypto.Ed25519.sign(message, w.privateKey.bytes)
+  )._toUplcData()
+);
 
-// TODO: Emulate several transactions & edge cases.
+const tx = new helios.Tx();
+tx.addInputs(await wallets[0].utxos);
+
+tx.attachScript(ismMultiSig);
+tx.mintTokens(
+  ismMultiSig.mintingPolicyHash,
+  [[helios.textToBytes("auth"), BigInt(1)]],
+  new helios.ListData([messageInUPLC, new helios.ListData(signatures)])
+);
+
+tx.addOutput(
+  new helios.TxOutput(
+    messageAddress,
+    new helios.Value(
+      BigInt(0), // Let Helios calculate the min ADA!
+      new helios.Assets([
+        [
+          ismMultiSig.mintingPolicyHash,
+          [[helios.textToBytes("auth"), BigInt(1)]],
+        ],
+      ])
+    ),
+    helios.Datum.inline(messageInUPLC)
+  )
+);
+
+await tx.finalize(new helios.NetworkParams(paramsPreview), wallets[0].address);
+const txId = await network.submitTx(tx);
+console.log(txId.hex);
+
+// TODO: Emulate several edge cases.
 // TODO: Post messages to Preview/Pre-Production addresses.
