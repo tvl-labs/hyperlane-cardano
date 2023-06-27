2b27f12ec5bf0687008c19d6f07a3aca54644f46
serejke <serejke.best@gmail.com>
2023-06-27 09:55:30 +0100

    feat: off-chain MerkleTree implementation and tests.

---
+++ src/merkle/h256.ts
@@ -0,0 +1,34 @@
+import { Buffer } from 'buffer';
+import assert from 'assert';
+
+export class H256 {
+
+  private buffer: Buffer;
+
+  private constructor(buffer: Buffer) {
+    this.buffer = buffer;
+  }
+
+  hex(): string {
+    return '0x' + this.buffer.toString('hex');
+  }
+
+  toBuffer(): Buffer {
+    const copy = Buffer.alloc(32);
+    this.buffer.copy(copy);
+    return copy;
+  }
+
+  toByteArray(): number[] {
+    return [...this.buffer.values()];
+  }
+
+  static from(buffer: Buffer) {
+    assert(buffer.length === 32, `Buffer size must be 32 bytes but was ${buffer.length}`);
+    return new H256(buffer);
+  }
+
+  static zero(): H256 {
+    return new H256(Buffer.alloc(32));
+  }
+}
---
+++ src/merkle/solidity.merkle.ts
@@ -0,0 +1,52 @@
+import { H256 } from './h256';
+import assert from 'assert';
+import { hashConcat, Hasher } from './hasher';
+import { MAX_LEAVES, TREE_DEPTH, zeroNode } from './common.merkle';
+
+// Hyperlane Solidity implementation of the MerkleTree
+//  https://github.com/hyperlane-xyz/hyperlane-monorepo/blob/50f04db1faddb6d471b85386bb977fe9762753df/solidity/contracts/libs/Merkle.sol#L11-L10
+export class SolidityMerkleTree {
+
+  private readonly hasher: Hasher;
+  private readonly branch: H256[]; // TREE_DEPTH size.
+  private count: number = 0;
+
+  constructor(hasher: Hasher) {
+    this.branch = new Array<H256>(TREE_DEPTH);
+    for (let i = 0; i < TREE_DEPTH; i++) {
+      this.branch[i] = H256.zero();
+    }
+    this.hasher = hasher;
+  }
+
+  ingest(node: H256) {
+    assert(this.count < MAX_LEAVES);
+    this.count++;
+    let size = this.count;
+    for (let i = 0; i < TREE_DEPTH; i++) {
+      if ((size & 1) === 1) {
+        this.branch[i] = node;
+        return;
+      }
+      node = hashConcat(this.hasher, this.branch[i], node);
+      size /= 2;
+    }
+    assert(false);
+  }
+
+  root(): H256 {
+    let index = this.count;
+    let current = H256.zero();
+    for (let i = 0; i < TREE_DEPTH; i++) {
+      const ithBit = (index >> i) & 0x01;
+      const next = this.branch[i];
+      if (ithBit === 1) {
+        current = hashConcat(this.hasher, next, current);
+      } else {
+        current = hashConcat(this.hasher, current, zeroNode(this.hasher, i))
+      }
+    }
+    return current;
+  }
+
+}
