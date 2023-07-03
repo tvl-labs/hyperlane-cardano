b55fb7f5867dc8ed53e3fb0e60f6281eef98466e
Hai | Kreate <hai@kreate.community>
2023-07-03 16:25:42 +0700

    refactor: format with prettier

--- src/merkle/hasher.ts
+++ src/merkle/hasher.ts
@@ -1,11 +1,12 @@
-import { Buffer } from 'buffer';
-import { H256 } from './h256';
-import keccak256 from 'keccak256';
-import { blake2bFinal, blake2bInit, blake2bUpdate } from 'blakejs';
+import { Buffer } from "buffer";
+import { H256 } from "./h256";
+import keccak256 from "keccak256";
+import { blake2bFinal, blake2bInit, blake2bUpdate } from "blakejs";
 
 export type Hasher = (Buffer) => H256;
 
-export const keccak256Hasher: Hasher = (data: Buffer) => H256.from(keccak256(data));
+export const keccak256Hasher: Hasher = (data: Buffer) =>
+  H256.from(keccak256(data));
 
 export function blake2bHasher(data: Buffer) {
   const ctx = blake2bInit(32);
@@ -14,10 +15,6 @@ export function blake2bHasher(data: Buffer) {
   return H256.from(Buffer.from(uint8Array));
 }
 
-export function hashConcat(
-  hasher: Hasher,
-  left: H256,
-  right: H256
-) {
+export function hashConcat(hasher: Hasher, left: H256, right: H256) {
   return hasher(Buffer.concat([left.toBuffer(), right.toBuffer()]));
 }
