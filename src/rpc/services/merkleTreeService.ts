3a96b80604b1f82e8d9ba4cff3cfc1e6894b3ac7
Hai | Kreate <hai@kreate.community>
2023-07-04 13:39:52 +0700

    feat: implement merkle tree rpc service

--- src/rpc/services/merkleTreeService.ts
+++ src/rpc/services/merkleTreeService.ts
@@ -1,10 +1,62 @@
+import * as helios from "@hyperionbt/helios";
+import fetch from "node-fetch";
+import ScriptOutbox from "../../onchain/scriptOutbox.hl";
 import type { MerkleTreesByBlockNumberResponseType } from "../types";
-import { getMerkleTreesAtBlockNumber } from "../mock/mock";
 
 export class MerkleTreeService {
   async getMerkleTreesAtBlockNumber(
     blockNumber: number
   ): Promise<MerkleTreesByBlockNumberResponseType> {
-    return getMerkleTreesAtBlockNumber(blockNumber);
+    const addressOutbox = helios.Address.fromValidatorHash(
+      new ScriptOutbox().compile(true).validatorHash
+    );
+
+    // TODO: Handle outbox identity
+    for (let page = 1; true; page++) {
+      const utxos: any = await fetch(
+        `${
+          process.env.BLOCKFROST_PREFIX ?? ""
+        }/addresses/${addressOutbox.toBech32()}/utxos?page=${page}&order=desc`,
+        {
+          headers: {
+            project_id: process.env.BLOCKFROST_PROJECT_ID ?? "",
+          },
+        }
+      ).then(async (r) => await r.json());
+
+      if (utxos.length === 0) break;
+
+      for (const utxo of utxos) {
+        const block: any = await fetch(
+          `${process.env.BLOCKFROST_PREFIX ?? ""}/blocks/${
+            utxo.block as string
+          }`,
+          {
+            headers: {
+              project_id: process.env.BLOCKFROST_PROJECT_ID ?? "",
+            },
+          }
+        ).then(async (r) => await r.json());
+
+        if (block.height <= blockNumber) {
+          const merkleTree = helios.ListData.fromCbor(
+            helios.hexToBytes(utxo.inline_datum)
+          ).list[0];
+          return {
+            blockNumber: block.height,
+            merkleTrees: [
+              {
+                count: Number(merkleTree.list[1].int),
+                branches: merkleTree.list[0].list.map((b) =>
+                  helios.bytesToHex(b.bytes)
+                ),
+              },
+            ],
+          };
+        }
+      }
+    }
+
+    return { blockNumber, merkleTrees: [] };
   }
 }
