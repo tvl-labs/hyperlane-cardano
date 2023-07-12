663c234426ad2040126fe7517ea7ab6241961f23
Hai | Kreate <hai@kreate.community>
2023-07-12 09:49:27 +0700

    fix: inbox test

--- src/test/testInbox.ts
+++ src/test/testInbox.ts
@@ -1,11 +1,16 @@
-import { waitForTxConfirmation } from '../offchain/waitForTxConfirmation';
-import { getInboundMessages } from '../offchain/indexer/getInboundMessages';
-import * as helios from '@hyperionbt/helios';
-import { emulatedNetwork, wallet } from '../../index';
+import { waitForTxConfirmation } from "../offchain/waitForTxConfirmation";
+import { getInboundMessages } from "../offchain/indexer/getInboundMessages";
+import * as helios from "@hyperionbt/helios";
+import { emulatedNetwork, wallet } from "../../index";
 import secp256k1 from "secp256k1";
-import ScriptLockForever from '../onchain/scriptLockForever.hl';
-import createInboundMessage from '../offchain/tx/createInboundMessage';
-import { blockfrost } from '../offchain/indexer/blockfrost';
+import ScriptLockForever from "../onchain/scriptLockForever.hl";
+import createInboundMessage from "../offchain/tx/createInboundMessage";
+import { blockfrost } from "../offchain/indexer/blockfrost";
+import { Address } from "../offchain/address";
+import { type Message, calculateMessageId } from "../offchain/message";
+import { MessagePayload } from "../offchain/messagePayload";
+import { DOMAIN_CARDANO } from "../rpc/mock/cardanoDomain";
+import { FUJI_DOMAIN } from "../rpc/mock/mockInitializer";
 
 // Mock inbound message
 const origin = Array(32).fill(0);
@@ -13,6 +18,19 @@ const originMailbox = Array(32).fill(1);
 const checkpointRoot = Array(32).fill(2);
 const checkpointIndex = Array(32).fill(3);
 const inboundMsg = `[${Date.now()}] Inbound Message!`;
+const inboundMessage: Message = {
+  version: 0,
+  nonce: 0,
+  originDomain: FUJI_DOMAIN,
+  sender: Address.fromHex(
+    "0x0000000000000000000000000000000000000000000000000000000000000EF1"
+  ),
+  destinationDomain: DOMAIN_CARDANO,
+  recipient: Address.fromHex(
+    "0x0000000000000000000000000000000000000000000000000000000000000CA1"
+  ),
+  message: MessagePayload.fromString(inboundMsg),
+};
 
 // TODO: Build several edge cases.
 
@@ -38,21 +56,20 @@ const appParams = {
 
 // TODO: Better interface & names here...
 async function createInboundMsg(blockfrost?: helios.BlockfrostV0) {
-  const message = helios.textToBytes(inboundMsg);
-  const messageHash = new Uint8Array(
+  const checkpointHash = new Uint8Array(
     helios.Crypto.blake2b(
       helios.Crypto.blake2b(
         origin.concat(originMailbox).concat(LABEL_HYPERLANE)
       )
         .concat(checkpointRoot)
         .concat(checkpointIndex)
-        .concat(helios.Crypto.blake2b(message))
+        .concat(calculateMessageId(inboundMessage).toByteArray())
     )
   );
   const signatures = ownerPrivateKeys.map(
     (k) =>
       new helios.ByteArray(
-        Array.from(secp256k1.ecdsaSign(messageHash, k).signature)
+        Array.from(secp256k1.ecdsaSign(checkpointHash, k).signature)
       )
   );
   return await createInboundMessage(
@@ -61,7 +78,7 @@ async function createInboundMsg(blockfrost?: helios.BlockfrostV0) {
     new helios.ByteArray(originMailbox),
     new helios.ByteArray(checkpointRoot),
     new helios.ByteArray(checkpointIndex),
-    new helios.ByteArray(message),
+    inboundMessage,
     signatures,
     wallet,
     blockfrost
