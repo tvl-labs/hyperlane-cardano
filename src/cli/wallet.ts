feb03237532c626d56fb823750a1454f56b6989f
Sergei Patrikeev <serejke.best@gmail.com>
2023-09-05 11:39:58 +0200

    cleanup: unused function.

--- src/cli/wallet.ts
+++ src/cli/wallet.ts
@@ -1,6 +1,5 @@
 import { Wallet } from "../offchain/wallet";
 import * as helios from "@hyperionbt/helios";
-import { H256 } from "../offchain/h256";
 
 export function createWallet(
   walletAddress: string | undefined = process.env.WALLET_ADDRESS,
@@ -16,20 +15,4 @@ export function createWallet(
     new helios.Address(walletAddress),
     new helios.PrivateKey(walletPrivateKey)
   );
-}
-
-export function getRecipientAddressAndHash() {
-  const recipientAddress = new helios.Address(
-    "addr_test1vpcvg34l9ngamtytg3ex5mxgcczgtu78dh3m3uxdk7cf5dg0scvn5"
-  );
-  const recipientAddressHash = H256.from(
-    Buffer.from(
-      helios.bytesToHex(helios.Crypto.blake2b(recipientAddress.bytes)),
-      "hex"
-    )
-  );
-  return {
-    recipientAddress,
-    recipientAddressHash,
-  };
 }
